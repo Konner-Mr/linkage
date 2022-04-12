6fedd24bfba8a09b0605ea3f8ca42387bacdc409
Konner-Mr <327637101@qq.com>
2022-04-12 10:33:07 +0800

    Add files via upload

--- index.vue
+++ index.vue
@@ -1,46 +1,48 @@
 <!--
-author: 康少
+author: Mr.Kon
 参数配置说明:
 [{
-    name: "",                                   //字段名称
-    getOptionUrl: ``,                           //获取下拉选项数据接口
-    linkage: false,                              //false => 一级级联字段init函数时获取下拉选项
-    params: { brandType: "vehicle" } || [{ key: "brand", params: { brandCode: "brandCode" } }], 
-                                                //获取数据时传参，对象：直接传参；数组：key是参数来源字段，params对象的key为参数名，value为数据来源的参数名，
-    showKey: [
-        {
-            key: ["series", "model"],
-            when: { brandType: "vehicle" }
-        },
-        {
-            key: ["assembleSeries", "assemblyModel"],
-            when: { brandType: "assembly_type" }
-        }
-    ],                                          //显示隐藏某些关联字段,key：关联字段； when：选中选项中brandType值为vehicle时显示关联字段，否则隐藏
-    change_key: ["series", "assembleSeries"],   //级联字段key值
     key: "brand",                               //当前字段key值
-    type: "select",                             //当前字段类型
+    name: "",                                   //字段名称
+    ………级联字段配置start………
+    getOptionUrl: ``,                           //获取下拉选项数据接口
+    linkage: false,                             //false => 一级级联字段init函数时获取下拉选项
+    params: [{ key: "brand", params: { brandCode: "brandCode" } }], 
+                                                //获取选项数据时传参，key是参数来源字段(对应字段配置中的key)，params对象中【属性名】为【传参名】，【属性值】为【key字段】下【选中选项的其他属性的值】，
+    default_params:{},                          //默认参数，和params合并传输
+    change_key: ["series"],                     //级联字段key值
+    empty_key:[],                               //级联字段，选项值为空的时候对其关联字段的option、value进行清空
+    type: "select",                             //当前字段类型 【input/select/date/dateTime】
     option: [],                                 //下拉选项
-    value: "",                                  //选中数据值
+    default_option:[],                          //默认选项，拼接在选项后面
     show_key: "brandName",                      //下拉选项显示的字段名
     value_key: "id",                            //下拉选项实际传输的值
+    noClearable: true,                          //不需要清空键，默认false
+    remote: true,                               //远程搜索开关
+    remoteMethod:remoteMethod,                  //v => 输入的值
+    ………级联字段end………
+    value: "",                                  //选中数据值
     search_key: "brandId",                      //传参时的key值
+    noEmpty: true,                              //初始化不需要清空value值
     show: true                                  //是否显示改字段，不显示时不传参
 }],
+远程搜索例子：函数内部可自定义，本案例中调用到组件的getIndex，getOption
+* remoteMethod => 自定义函数名
+* e => 输入的值
+* key => 字段名称
+const remoteMethod = (e, key = 'series') => {
+  let index = linkage.value.getIndex(key)
+  linkage.value.getOption('knowledge/inner/brand/query', { brandType: 'vehicle,assembly_type' }, index)
+}
 -->
 
 <template>
   <div class="filterWrap">
-    <div
-      style="display:inline-block"
-      class="px-1 mb-1"
-      v-for="(item, index) in filterArray"
-      :key="index"
-      v-show="item.show"
-    >
+    <div style="display: inline-block" class="px-1 mb-1" v-for="(item, index) in field" :key="index" v-show="item.show">
       <div class="rowSC" :style="{ width: item.width ? item.width : 'auto' }">
         <div class="mr-2" style="white-space: nowrap">
-          <span class="text-danger" v-if="item.must">*</span>{{ item.name }}
+          <span class="text-danger" v-if="item.must">*</span>
+          {{ item.name }}
         </div>
         <template v-if="item.type == 'select'">
           <el-select
@@ -48,7 +50,9 @@ author: 康少
             filterable
             v-model="item.value"
             placeholder="请选择"
-            clearable
+            :remote="item.remote"
+            :remote-method="item.remoteMethod"
+            :clearable="!item.noClearable ? true : false"
             @change="selectChange(item)"
           >
             <el-option
@@ -56,8 +60,7 @@ author: 康少
               :key="o_index"
               :label="o_item[item.show_key]"
               :value="o_item[item.value_key]"
-            >
-            </el-option>
+            ></el-option>
           </el-select>
         </template>
         <template v-if="item.type == 'input'">
@@ -68,205 +71,171 @@ author: 康少
             clearable
           ></el-input>
         </template>
-        <template v-if="item.type == 'time'">
+        <template v-if="item.type == 'date'">
           <el-date-picker
             v-model="item.value"
             type="daterange"
-            value-format="yyyy-MM-dd"
+            value-format="YYYY-MM-DD"
             range-separator="至"
             start-placeholder="开始日期"
             end-placeholder="结束日期"
-          >
-          </el-date-picker>
+          ></el-date-picker>
+        </template>
+        <template v-if="item.type == 'dateTime'">
+          <el-date-picker
+            v-model="item.value"
+            type="datetimerange"
+            value-format="YYYY-MM-DD hh:mm:ss"
+            range-separator="至"
+            start-placeholder="开始日期"
+            end-placeholder="结束日期"
+            :disabled-date="disabledDate"
+          ></el-date-picker>
         </template>
       </div>
     </div>
   </div>
 </template>
 
-<script>
-export default {
-  props: {
-    /**
-     * 级联过滤字段**/
-    field: {
-      type: Array,
-      default: () => []
-    },
-    /**
-     * 是否直接初始化**/
-    autoInit: {
-      type: Boolean,
-      default: false
-    }
+<script setup>
+import request from '@/utils/axiosReq'
+import { defineExpose } from 'vue'
+const props = defineProps({
+  /**
+   * 级联过滤字段**/
+  field: {
+    type: Array,
+    default: () => []
   },
-  data() {
-    return {
-      filterArray: []
-    };
-  },
-  mounted() {
-    if (this.autoInit) this.init();
-  },
-  watch: {
-    /**
-     * 监听字段变化返回参数集合**/
-    filterArray: {
-      handler(newV, oldV) {
-        let params = new Object();
-        newV.forEach(item => {
-          if (item.search_key && item.show) {
-            if (
-              Object.prototype.toString.call(item.search_key) ==
-              "[object Array]"
-            ) {
-              item.search_key.forEach((o_item, index) => {
-                params[o_item] = item.value ? item.value[index] : "";
-              });
-            } else {
-              params[item.search_key] = item.value;
-            }
-          }
-        });
-        this.$emit("paramsCall", params);
-      },
-      immediate: true,
-      deep: true
-    }
-  },
-  methods: {
-    /**
-     * 初始化**/
-    init() {
-      this.filterArray = this.field;
-      return new Promise((resolve, reject) => {
-        this.$nextTick(() => {
-          let filter = this.filterArray,
-            getArray = new Array();
-          filter.forEach((item, index) => {
-            if (item.getOptionUrl) {
-              item.option = [];
-            }
-            item.value = item.type != "checkbox" ? "" : [];
-            if (!item.linkage && item.getOptionUrl) {
-              getArray.push(
-                this.getOption(item.getOptionUrl, item.params, index)
-              );
-            }
-          });
-          if (getArray?.length > 0) {
-            Promise.all(getArray).then(() => {
-              resolve();
-            });
-          } else {
-            resolve();
-          }
-        });
-      });
-    },
-    /**
-     * 获取下拉字段选项数据**/
-    getOption(url, data, index) {
-      return new Promise((resolve, reject) => {
-        this.$set(this.filterArray[index], "value", "");
-        this.$axios({
-          url,
-          method: "get",
-          data
-        })
-          .then(res => {
-            this.$set(
-              this.filterArray[index],
-              "option",
-              res.data.records || res.data
-            );
-            resolve();
-          })
-          .catch(() => {
-            resolve();
-          });
-      });
-    },
-    /**
-     * 下拉选项触发change事件**/
-    selectChange(e) {
-      let o_index = e.option.findIndex(i => e.value == i[e.value_key]),
-        filterArray = this.filterArray;
-      //处理显示隐藏关联字段
-      if (e.showKey) {
-        this.show_hide_field(e, e.option[o_index]);
-      }
-      //处理联动查询
-      if (e.change_key) {
-        let change_key = e.change_key;
-        change_key.forEach(c_item => {
-          let index = filterArray.findIndex(c => c_item == c.key);
-          //联动字段show为true
-          if (index !== -1 && filterArray[index].show) {
-            if (e.value !== "") {
-              let url = filterArray[index].getOptionUrl,
-                params = new Object(),
-                config_params = filterArray[index].params;
-              config_params.forEach(config_item => {
-                let config_key = config_item.key,
-                  config_child_params = config_item.params,
-                  config_index = filterArray.findIndex(
-                    c => config_key == c.key
-                  );
-                if (config_index !== -1) {
-                  let params_index = filterArray[config_index].option.findIndex(
-                    p_item =>
-                      p_item[filterArray[config_index].value_key] ==
-                      filterArray[config_index].value
-                  );
-                  for (let x in config_child_params) {
-                    params[x] =
-                      filterArray[config_index].option[params_index][
-                        config_child_params[x]
-                      ];
-                  }
-                }
-              });
-              this.getOption(url, params, index);
-            } else {
-              //清除关联字段选项和值
-              this.$set(this.filterArray[index], "value", "");
-              this.$set(this.filterArray[index], "option", []);
-            }
-          }
-        });
-      }
-    },
-    /**
-     * 下拉选项触发关联字段显示隐藏**/
-    show_hide_field(e, o_item) {
-      e.showKey.forEach(item => {
-        let key = item.key,
-          when = item.when;
-        for (let x in when) {
-          //如果选中字段x == 指定值，则key中字段显示，否则隐藏，
-          key.forEach(k_item => {
-            let index = this.filterArray.findIndex(
-              f_item => f_item.key === k_item
-            );
-            if (index != -1) {
-              if (e.value !== "") {
-                this.$set(
-                  this.filterArray[index],
-                  "show",
-                  o_item[x] == when[x] ? true : false
-                );
-              }
-              //清除所有关联字段选项和值
-              this.$set(this.filterArray[index], "value", "");
-              this.$set(this.filterArray[index], "option", []);
-            }
-          });
-        }
-      });
-    }
+  /**
+   * 是否直接初始化**/
+  autoInit: {
+    type: Boolean,
+    default: false
   }
-};
+})
+const emits = defineEmits(['paramsCall'])
+
+//禁止选择的日期
+const disabledDate = (time) => {
+  return time.getTime() > Date.now() - 8.64e6
+}
+
+/**
+ * 通过watch把参数集合组装好抛出去给父组件
+ */
+watch(
+  () => props.field,
+  (newV, oldV) => {
+    let params = new Object()
+    newV.forEach((item) => {
+      if (item.search_key && item.show) {
+        if (Object.prototype.toString.call(item.search_key) == '[object Array]') {
+          item.search_key.forEach((o_item, index) => {
+            params[o_item] = item.value ? item.value[index] : ''
+          })
+        } else {
+          params[item.search_key] = item.value
+        }
+      }
+    })
+    emits('paramsCall', params)
+  },
+  {
+    deep: true
+  }
+)
+
+/**
+ * 初始化
+ */
+const init = () => {
+  return new Promise(async (resolve, reject) => {
+    let f = props.field
+    for (let x = 0; x < f.length; x++) {
+      if (f[x].getOptionUrl) f[x].option = []
+      if (f[x].noEmpty) f[x].value = f[x].type != 'checkbox' ? '' : []
+      if (!f[x].linkage && f[x].getOptionUrl) {
+        await getOption(f[x].getOptionUrl, f[x].default_params, x)
+      }
+      if (x == f.length - 1) resolve()
+    }
+  })
+}
+
+/**
+ * 获取下拉字段选项数据**/
+const getOption = (url, params, index, method = 'get') => {
+  return new Promise((resolve, reject) => {
+    props.field[index].value = ''
+    request({ url, method, data: params, isParams: true })
+      .then((res) => {
+        let data = res.data.records || res.data,
+          default_option = props.field[index].default_option
+        props.field[index].option = default_option ? [...data, ...default_option].flat() : data
+        resolve()
+      })
+      .catch(() => {
+        resolve()
+      })
+  })
+}
+
+/**
+ * 下拉选择
+ */
+const selectChange = (e) => {
+  let f = props.field
+  if (e.change_key) {
+    e.change_key.forEach((a) => {
+      //关联字段索引值
+      let index = f.findIndex((c) => a == c.key)
+      if (index !== -1) {
+        if (e.value !== '') {
+          let url = f[index].getOptionUrl,
+            params = new Object(),
+            config_params = f[index].params
+          config_params.forEach((config_item) => {
+            let config_key = config_item.key,
+              config_child_params = config_item.params,
+              config_index = f.findIndex((c) => config_key == c.key)
+            if (config_index !== -1) {
+              let params_index = f[config_index].option.findIndex(
+                (p_item) => p_item[f[config_index].value_key] == f[config_index].value
+              )
+              for (let x in config_child_params) {
+                params[x] = f[config_index].option[params_index][config_child_params[x]]
+              }
+            }
+          })
+          if (f[index].default_params) params = Object.assign(f[index].default_params, params)
+          getOption(url, params, index)
+        } else {
+          let e_key = e.empty_key
+          if (e_key) {
+            e_key.forEach((e) => {
+              let e_i = f.findIndex((c) => e == c.key)
+              if (e_i !== -1) {
+                f[e_i].option = f[e_i].default_option ? f[e_i].default_option : []
+                f[e_i].value = ''
+              }
+            })
+          }
+        }
+      }
+    })
+  }
+}
+
+/**
+ * 获取字段在props.field中的索引值
+ * key不能为空
+ */
+const getIndex = (key) => {
+  return props.field.findIndex((item) => item.key === key)
+}
+
+defineExpose({ init, getOption, getIndex })
 </script>
 
 <style lang="scss" scoped>
